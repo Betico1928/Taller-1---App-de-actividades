0b146803def4d0957a1b491e5c776e4757dcbad4
Betico1928 <betoveluis@gmail.com>
2023-02-25 00:58:03 -0500

    Inicializados todos los botones y generadas las funciones.

--- app/src/main/java/javeriana/edu/co/taller1_appdeactividades/MainActivity.kt
+++ app/src/main/java/javeriana/edu/co/taller1_appdeactividades/MainActivity.kt
@@ -17,14 +17,40 @@ class MainActivity : AppCompatActivity()
         bindingMain = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingMain.root)
 
+        inicializarBotones()
+    }
+
+    fun inicializarBotones()
+    {
         // Guessing Number
         bindingMain.guessGameButton.setOnClickListener{
-            Log.i("onCreate", "Presionar boton")
+            Log.i("Inicializar botones", "Ir a guessingNumbers")
             guessingNumbers()
         }
 
-        //
+        // Factorial
+        bindingMain.factorialButton.setOnClickListener{
+            Log.i("Inicializar botones", "Ir a Factorial")
+            factorial()
+        }
+
+        bindingMain.fibonacciButton.setOnClickListener{
+            Log.i("Inicializar botones", "Ir a Fibonacci")
+            fibonacci()
+        }
+
+        bindingMain.randomGreetButton.setOnClickListener{
+            Log.i("Inicializar botones", "Ir a random Greet")
+            randomGreet()
+        }
+
+        bindingMain.countriesButton.setOnClickListener{
+            Log.i("Inicializar botones", "Ir a countries")
+            countries()
+        }
     }
+
+
 
     fun guessingNumbers()
     {
@@ -33,6 +59,37 @@ class MainActivity : AppCompatActivity()
         var numeroIngresado = bindingMain.numeroAdivinadoText.text
         Log.i("Guessing Numbers", "Imprimir numero" + numeroIngresado)
 
-        // TODO: Ir a la siguiente actividad (GuessGame)
+        // TODO: Ir a la siguiente actividad (GuessGame) y verificar tamaño del numero
+    }
+
+
+    fun factorial()
+    {
+        Log.i("Factorial", "Entrada")
+
+        // TODO: Ir a la siguiente actividad (FactorialActivity)
+    }
+
+
+    fun fibonacci()
+    {
+        Log.i("Fibonacci", "Entrada")
+
+        // TODO: Ir a la siguiente actividad (FibonacciActivity)
+    }
+
+
+    fun randomGreet()
+    {
+        Log.i("Random Greet", "Entrada")
+
+        // TODO: Verificar el idioma seleccionado en el spinner y pasar a la actividad (RandomGreet)
+    }
+
+    fun countries()
+    {
+        Log.i("Countries", "Entrada")
+
+        // TODO: Pasar a la siguiente actividad (CountriesActivity)
     }
 }
