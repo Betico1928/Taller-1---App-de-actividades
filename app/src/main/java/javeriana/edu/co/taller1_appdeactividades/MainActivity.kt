c959770e384f397906b91e5dac00aaf76064cf9b
Betico1928 <betoveluis@gmail.com>
2023-02-25 00:17:07 -0500

    Creada la entrada a guessingNumbers y recibir su numero.

--- app/src/main/java/javeriana/edu/co/taller1_appdeactividades/MainActivity.kt
+++ app/src/main/java/javeriana/edu/co/taller1_appdeactividades/MainActivity.kt
@@ -2,10 +2,37 @@ package javeriana.edu.co.taller1_appdeactividades
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import javeriana.edu.co.taller1_appdeactividades.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var bindingMain : ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMain.root)
+
+        // Guessing Number
+        bindingMain.guessGameButton.setOnClickListener{
+            Log.i("onCreate", "Presionar boton")
+            guessingNumbers()
+        }
+
+        //
+    }
+
+    fun guessingNumbers()
+    {
+        Log.i("Guessing Numbers", "Entrada")
+
+        var numeroIngresado = bindingMain.numeroAdivinadoText.text
+        Log.i("Guessing Numbers", "Imprimir numero" + numeroIngresado)
+
+        // TODO: Ir a la siguiente actividad (GuessGame)
     }
 }
