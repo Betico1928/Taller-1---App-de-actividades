4d7b40b75a5a567331d9792fcb26cac0771f9390
Betico1928 <betoveluis@gmail.com>
2023-02-24 23:24:44 -0500

    Añadidas todas las actividades y el buildFeatures

---
+++ app/src/main/java/javeriana/edu/co/taller1_appdeactividades/FactorialActivity.kt
@@ -0,0 +1,11 @@
+package javeriana.edu.co.taller1_appdeactividades
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FactorialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_factorial)
+    }
+}
---
+++ app/src/main/java/javeriana/edu/co/taller1_appdeactividades/RandomGreet.kt
@@ -0,0 +1,11 @@
+package javeriana.edu.co.taller1_appdeactividades
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RandomGreet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random_greet)
+    }
+}
