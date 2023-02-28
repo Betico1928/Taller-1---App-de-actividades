53d2602eddb9f3db697ffef46039edb40a7b4e2c
Betico1928 <betoveluis@gmail.com>
2023-02-27 19:47:06 -0500

    DIOS MIO SANTO PADRE GRACIAS TE AMO TE AMO MUCHO 🙏🏻⛪️

--- app/src/main/java/javeriana/edu/co/taller1_appdeactividades/MostrarPaisActivity.kt
+++ app/src/main/java/javeriana/edu/co/taller1_appdeactividades/MostrarPaisActivity.kt
@@ -1,12 +1,103 @@
 package javeriana.edu.co.taller1_appdeactividades
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.google.gson.Gson
+import com.google.gson.JsonElement
+import com.google.gson.JsonParser
+import com.google.gson.reflect.TypeToken
 import javeriana.edu.co.taller1_appdeactividades.databinding.ActivityMostrarPaisBinding
+import org.json.JSONObject
+import java.io.*
+
+data class Pais(
+    val capital: String,
+    val nombre_pais: String,
+    val nombre_pais_int: String,
+    val sigla: String
+)
+
 
 class MostrarPaisActivity : AppCompatActivity()
 {
+    private lateinit var bindingMostrarPais: ActivityMostrarPaisBinding
+
+
+    fun copyJsonToInternalStorage(context: Context, fileName: String) {
+        val inputStream = context.assets.open(fileName)
+        val outputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE)
+        inputStream.copyTo(outputStream)
+        inputStream.close()
+        outputStream.close()
+    }
+
+    fun buscarPaisPorNombre(context: Context, nombrePais: String): JSONObject? {
+        val fileName = "paises.json"
+        copyJsonToInternalStorage(context, fileName)
+
+        val fileInputStream = context.openFileInput(fileName)
+        val bufferedReader = BufferedReader(InputStreamReader(fileInputStream))
+        val jsonString = bufferedReader.readText()
+
+        val jsonObject = JSONObject(jsonString)
+        val paises = jsonObject.getJSONArray("paises")
+
+        for (i in 0 until paises.length()) {
+            val pais = paises.getJSONObject(i)
+            if (pais.getString("nombre_pais") == nombrePais) {
+                return pais
+            }
+        }
+
+        return null
+    }
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        bindingMostrarPais = ActivityMostrarPaisBinding.inflate(layoutInflater)
+        setContentView(bindingMostrarPais.root)
+
+        // Recibir pais seleccionado
+        var nombrePais = intent.getStringExtra("paisSeleccionado")
+        Log.i("Mostrar Pais", "Pais Recibido: $nombrePais")
+
+        val pais = nombrePais?.let { buscarPaisPorNombre(this, it) }
+
+        if (pais != null) {
+            val capital = pais.getString("capital")
+            val nombrePais = pais.getString("nombre_pais")
+            val nombrePaisInt = pais.getString("nombre_pais_int")
+            val sigla = pais.getString("sigla")
+
+            bindingMostrarPais.capital.text = capital
+            bindingMostrarPais.nombrePais.text = nombrePais
+            bindingMostrarPais.nombrePaisInt.text = nombrePaisInt
+            bindingMostrarPais.sigla.text = sigla
+        } else {
+            Toast.makeText(this, "País no encontrado", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
     private lateinit var bindingMostrarPais: ActivityMostrarPaisBinding
 
 
@@ -17,9 +108,94 @@ class MostrarPaisActivity : AppCompatActivity()
         setContentView(bindingMostrarPais.root)
 
         // Recibir pais seleccionado
-        var paisSeleccionado = intent.getStringExtra("paisSeleccionado")
-        Log.i("Mostrar Pais", "Pais Recibido: $paisSeleccionado")
+        var nombrePais = intent.getStringExtra("paisSeleccionado")
+        Log.i("Mostrar Pais", "Pais Recibido: $nombrePais")
 
-        bindingMostrarPais.nombrePais.text = paisSeleccionado
+        bindingMostrarPais.nombrePais.text = nombrePais
+
+        var nombrePaisString : String = nombrePais.toString()
+
+
+        val pais = buscarPais("Alemania")
+        if (pais != null) {
+            with(bindingMostrarPais) {
+                tvCapital.text = pais.capital
+                tvNombrePais.text = pais.nombre_pais
+                tvNombrePaisInt.text = pais.nombre_pais_int
+                tvSigla.text = pais.sigla
+            }
+        }
+
     }
+
+    fun buscarPais(context: Context, nombrePais: String): Pais? {
+        val json = obtenerJsonDesdeArchivo(context, "paises.json")
+        val gson = Gson()
+        val listaPaises = gson.fromJson(json, Array<Pais>::class.java)
+
+        for (pais in listaPaises) {
+            if (pais.nombre_pais == nombrePais) {
+                return pais
+            }
+        }
+
+        return null
+    }
+
+
+    /*
+        val paisBuscado = buscarPais("Argentina")
+        if (paisBuscado != null) {
+            println("Capital: ${paisBuscado.capital}")
+            println("Nombre del país: ${paisBuscado.nombre_pais}")
+            println("Nombre del país en inglés: ${paisBuscado.nombre_pais_int}")
+            println("Sigla: ${paisBuscado.sigla}")
+        } else {
+            println("El país buscado no se encuentra en la lista")
+        }
+
+
+
+    fun obtenerDetallesDelPais(nombrePais: String, context: Context): Pais? {
+        val gson = Gson()
+        val inputStream: InputStream = context.assets.open("paises.json")
+        val size: Int = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+        val paisesJson = gson.fromJson(String(buffer), Paises::class.java)
+        return paisesJson.paises.find { it.nombre_pais == nombrePais }
+    }
+
+     */
+
+
+    /*
+    fun buscarEnElJSON(paisSeleccionado : String)
+    {
+        val pais = buscarPais(this, paisSeleccionado)
+
+        if (pais != null) {
+            println("Capital: ${pais.capital}")
+            println("Nombre del país: ${pais.nombre_pais}")
+            println("Nombre internacional: ${pais.nombre_pais_int}")
+            println("Sigla: ${pais.sigla}")
+        } else {
+            println("El país no se encuentra en la lista.")
+        }
+    }
+
+    private fun buscarPais(context: MostrarPaisActivity, nombrePais: String): Pais? {
+        val gson = Gson()
+        val inputStream = context.assets.open("paises.json")
+        val reader = InputStreamReader(inputStream)
+        val paises = gson.fromJson(reader, Array<Pais>::class.java)
+
+        return paises.find { it.nombre_pais == nombrePais }
+    }
+
+     */
+}
+
+     */
 }
